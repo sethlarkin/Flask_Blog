de8ba07ec4f0733fff9e07bea3e181599ffa441b
sethlarkin <sethlarkin@gmail.com>
2018-11-15 06:03:19 -0800

    updates regristration form and validation

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegristrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '9e88cbb84d21b4716b87a2c26fc85f12'
 
 posts = [
     {
@@ -18,7 +21,7 @@ posts = [
 
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('home.html', posts=posts)
 
 
@@ -26,6 +29,20 @@ def hello_world():
 def about():
     return render_template('about.html', title='About')
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegristrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
